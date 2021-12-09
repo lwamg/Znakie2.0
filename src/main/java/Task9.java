1d3f706246b8f94d5942aff54ffec7b9b78d7900
Lwam Ghebresus <lwam.ghebresus@hotmail.com>
2021-12-09 10:27:12 -0800

    clean

--- src/main/java/Task9.java
+++ src/main/java/Task9.java
@@ -44,9 +44,8 @@ public class Task9 {
 
 
                 KeyType type = keyStroke.getKeyType();
-                Character c = keyStroke.getCharacter(); // used Character, not char because it might be null
-                System.out.println("keyStroke.getKeyType(): " + type
-                        + " keyStroke.getCharacter(): " + c);
+                Character c = keyStroke.getCharacter();
+
 
                 if (c == Character.valueOf('q')) {
                     continueReadingInput = false;
@@ -55,72 +54,78 @@ public class Task9 {
 
                 }
 
-                if (x == xMonster && y == yMonster) {
-                    continueReadingInput = false;
-                    System.out.println("GAME OVER");
-                    String g = "GAME OVER \t WINNER \u0394 \t LOSER X";
-                    for (int i = 0; i < g.length(); i++) {
-                        terminal.setCursorPosition(30+i, 10);
-                        terminal.putCharacter(g.charAt(i));
-
-
-                    }
-                }
+//                if (x == xMonster && y == yMonster) {
+//                    continueReadingInput = false;
+//                    System.out.println("GAME OVER");
+//                    String g = "GAME OVER \t WINNER \u0394 \t LOSER X";
+//                    for (int i = 0; i < g.length(); i++) {
+//                        terminal.setCursorPosition(30+i, 10);
+//                        terminal.putCharacter(g.charAt(i));
+//
+//
+//                    }
+//                }
                 int oldX=x;
                 int oldY=y;
 
-                int oldXMonster= xMonster;
-                int oldYMonster= yMonster;
+//                int oldXMonster= xMonster;
+//                int oldYMonster= yMonster;
 
 
 
                 switch (keyStroke.getKeyType()) {
                     case ArrowDown:
                         y += 1;
-                        if(yMonster<=y){
-                        yMonster +=1;}
-                        else  if (yMonster>=y){
-                            yMonster-=2;
-                        }
+//                        if(yMonster<=y){
+//                        yMonster +=1;}
+//                        else  if (yMonster>=y){
+//                            yMonster-=2;
+//                        }
                         break;
                     case ArrowUp:
                         y -= 1;
-                        if(yMonster<=y){
-                            yMonster +=1;}
-                        else  if (yMonster>=y) {
-                            yMonster -= 2;
-                        }
+//                        if(yMonster<=y){
+//                            yMonster +=1;}
+//                        else  if (yMonster>=y) {
+//                            yMonster -= 2;
+//                        }
                         break;
                     case ArrowRight:
                         x += 1;
-                        if(xMonster <= x) {
-                            xMonster += 2;
-                        }
-                        else if(xMonster >= x){
-                            xMonster -=2;
-                        }
+//                        if(xMonster <= x) {
+//                            xMonster += 2;
+//                        }
+//                        else if(xMonster >= x){
+//                            xMonster -=2;
+//                        }
                         break;
                     case ArrowLeft:
                         x -= 1;
-                        if(xMonster <= x) {
-                            xMonster += 1;
-                        }
-                        else if(xMonster >= x){
-                            xMonster -=2;
-                        }
+//                        if(xMonster <= x) {
+//                            xMonster += 1;
+//                        }
+//                        else if(xMonster >= x){
+//                            xMonster -=2;
+//                        }
                         break;
 
                 }
 
+                if (x == xMonster && y == yMonster) {
+                    terminal.setCursorPosition(x, y);
+                    terminal.putCharacter(player);
+
+                }
+                else
                 terminal.setCursorPosition(oldX,oldY);
                 terminal.putCharacter(' ');
                 terminal.setCursorPosition(x,y);
                 terminal.putCharacter(player);
 
-                terminal.setCursorPosition(oldXMonster,oldYMonster);
-                terminal.putCharacter(' ');
-                terminal.setCursorPosition(xMonster,+yMonster);
-                terminal.putCharacter(monster);
+//                terminal.setCursorPosition(oldXMonster,oldYMonster);
+//                terminal.putCharacter(' ');
+//                terminal.setCursorPosition(xMonster,+yMonster);
+//                terminal.putCharacter(monster);
 
 
         terminal.flush();
