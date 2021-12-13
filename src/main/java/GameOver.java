8d3c56c16482ca904125444cb819af4669da1ea1
Lwam Ghebresus <lwam.ghebresus@hotmail.com>
2021-12-13 10:12:13 -0800

    Med score points och ny framemetod

--- src/main/java/GameOver.java
+++ src/main/java/GameOver.java
@@ -7,8 +7,8 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 public class GameOver {
 
-    public static void gameOver(Terminal terminal)throws Exception {
-        String g = "GAME OVER \t WINNER \u0394 \t LOSER X";
+    public static void gameOver(Terminal terminal, int score)throws Exception {
+        String g = "GAME OVER! You scored " + score;
         for (int i = 0; i < g.length(); i++) {
             terminal.setCursorPosition(30 + i, 10);
             terminal.putCharacter(g.charAt(i));
